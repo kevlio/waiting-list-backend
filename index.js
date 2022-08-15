2009bbebc91a126b740f68e74fb0607d611b81e8
cme-osuka <98716814+cme-osuka@users.noreply.github.com>
2022-08-15 12:38:08 +0100

    Added Frontend URL to CORS origins

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const knex = require("./db");
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://admin.socket.io", "http://localhost:3000", "https://amritb.github.io/"],
+    origin: ["https://admin.socket.io", "http://localhost:3000", "https://amritb.github.io/", "https://osuka-waitinglist.herokuapp.com/"],
     credentials: true
   },
 });
